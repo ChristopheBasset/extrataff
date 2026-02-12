190022b4a54aba50620df41e526a8fc0067d2afc
ChristopheBasset <christophe@comunecom.fr>
2026-02-12 18:45:30 +0100

    package et main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { PostHogProvider } from 'posthog-js/react'
 import App from './App'
 import './index.css'
 import { registerServiceWorker } from './lib/pushNotifications'
 
+const posthogOptions = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+  person_profiles: 'identified_only',
+  capture_pageview: true,
+  capture_pageleave: true,
+  session_recording: {
+    recordCrossOriginIframes: false,
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY || 'phc_VlohRlSDbOBDS8T6ptmWDSEB825u8P7koun97aPYH9j'}
+      options={posthogOptions}
+    >
+      <App />
+    </PostHogProvider>
   </React.StrictMode>,
 )
 
